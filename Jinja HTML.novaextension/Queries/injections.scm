8980fed44402ead4384b9e42d4883be49479f518
Timothy Rezendes <78620355+TRezendes@users.noreply.github.com>
2025-05-23 15:33:09 -0400

    It’s ALIVE!!! Highlighting works! Successfully highlights Jinha syntax, *and* python within Jinja tags. Mostly I am happy with the state of the highlighter. It may still need some tweaks, but I will need to dogfood it for a little while to see where it still needs work

--- Jinja HTML.novaextension/Queries/injections.scm
+++ Jinja HTML.novaextension/Queries/injections.scm
@@ -1,3 +1,25 @@
-((content) @injection.content
- (#set! injection.language "html")
- (#set! injection.combined))
+(
+  (words) @injection.content
+  (#set! injection.language "html")
+  (#set! injection.combined)
+)
+
+(
+  (control
+    "{%" @injection.content.start
+      (_)? @control.body
+    "%}" @injection.content.end)
+  (#not-contains? @control.body "extends" "block" "include")
+  (#not-match? @control.body "(?i)^\s?\bend\w+\s?")
+  (#set! injection.language "python")
+  (#set! injection.combined)
+)
+
+(
+  (render_expression
+    "{{" @injection.content.start
+      (_)? @expression.body
+    "}}" @injection.content.end)
+  (#not-contains? @control.body "|")
+  (#set! injection.language "python")
+)
